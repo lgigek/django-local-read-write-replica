8c513b694c0497f738707a8cefd6b16f570dfed3
lgigek <lucasgigek@gmail.com>
2021-03-26 20:03:11 -0300

    created new model

--- django_local_read_write_replica/apps/my_app/models.py
+++ django_local_read_write_replica/apps/my_app/models.py
@@ -1,1 +1,16 @@
+from uuid import uuid4
+
 from django.db import models
+
+
+class StandardModelMixin(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False, verbose_name="ID")
+    created_at = models.DateTimeField(auto_now_add=True, editable=False, verbose_name="Created at")
+    updated_at = models.DateTimeField(auto_now=True, editable=False, verbose_name="Updated at")
+
+    class Meta:
+        abstract = True
+
+
+class Message(StandardModelMixin):
+    text: models.CharField(max_length=200, blank=False, null=False)
