6a46c4d60bf0755ab9cc4cdd9ff0e4bec01b9710
lgigek <lucasgigek@gmail.com>
2021-04-05 16:33:22 -0300

    fixed serializer class

--- django_local_read_write_replica/apps/my_app/drf/serializers.py
+++ django_local_read_write_replica/apps/my_app/drf/serializers.py
@@ -9,12 +9,12 @@ from django_local_read_write_replica.apps.my_app.models import Message
 logger = logging.getLogger(__name__)
 
 
-class NewMessageSerializer(serializers.SerializerMethodField):
+class NewMessageSerializer(serializers.Serializer):
 
     text = serializers.CharField(max_length=200)
 
     @staticmethod
-    def create(validated_data: Dict) -> Message:
+    def create(validated_data: Dict, **kwargs) -> Message:
         with transaction.atomic():  # <- created useless transaction to force read/write evidences
             created_message = Message.objects.create(text=validated_data["text"])
             logger.info("Message created with id: %s", created_message.id)
