d33e522398396c2b00db27e35ec3ef9a980160a4
lgigek <lucasgigek@gmail.com>
2021-04-05 16:33:41 -0300

    finished route logic and added it on urls

--- django_local_read_write_replica/apps/my_app/drf/views.py
+++ django_local_read_write_replica/apps/my_app/drf/views.py
@@ -1,10 +1,11 @@
 import logging
 
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED
 from rest_framework.viewsets import ModelViewSet
 
-from django_local_read_write_replica.apps.my_app.drf.serializers import \
-    NewMessageSerializer
+from django_local_read_write_replica.apps.my_app.drf.serializers import NewMessageSerializer
 from django_local_read_write_replica.apps.my_app.models import Message
 
 logger = logging.getLogger(__name__)
@@ -25,4 +26,6 @@ class MessageViewSet(ModelViewSet):
             raise ValidationError(serializer.errors)
 
         logger.info("Data is valid, saving Message...")
-        serializer.create(serializer.validated_data)
+        created_message = serializer.create(serializer.validated_data)
+
+        return Response({"id": str(created_message.id)}, HTTP_201_CREATED)
